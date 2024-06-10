c3d97a591e8f6fe1faa82b0fe9944e0e2734465c
Mary Steineman <14steinemanm@gmail.com>
2024-06-09 22:54:56 -0500

    <debugginglogic3

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -2,7 +2,10 @@
 // Run the code and examine the output. 
 
 // Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
+   // Should be false on both accounts, but it's false the first time, and true the second time.  
 // Is the program behaving as expected?
+   // Yes, for the first launchReady, if/else statement.  No, for the second one.  True is still not the same thing as 'green'.  Although, according to pt. 5 of this, that apparently doesn't matter because it states that the problem is that the launchReady variable is being changed.  So apparently true is === 'green' somehow???
+   // Are these separate statements somehow?  Is it saying, "'If crewStatus = true' and 'If 'computerStatus = green' is true', then go cuz they're both true?"  If so, how are we supposed to tell the difference between that being the intention and the intention being "'If crewStatus = green' and 'if computerStatus = green' are both true, then go"??
 
 let launchReady = false;
 let fuelLevel = 17000;
